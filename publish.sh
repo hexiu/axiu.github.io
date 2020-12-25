6350ab20f15cba9b25828c5cb58412cb2943ed45
云凡 <hejiahao@innotechx.com>
2020-12-25 14:53:22 +0800

    fix: 20201225-1453发布新文章

---
+++ publish.sh
@@ -0,0 +1,15 @@
+###
+ # @Date: 2020-12-25 14:47:43
+ # @LastEditors: jaxiu
+ # @LastEditTime: 2020-12-25 14:53:00
+ # @FilePath: /axiu.github.io/publish.sh
+### 
+
+#!/bin/bash 
+# 
+hugo server --theme=AllinOne -D
+rm -rf ../hexiu.github.io/*
+cp -rp ./public/* ../hexiu.github.io/ 
+git add .
+git commit -m "fix: `date +%Y%m%d-%H%M`发布新文章"
+git push origin master
